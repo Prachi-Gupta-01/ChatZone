301e30971b90c9c778e96d3b2f6c617a88b91604
Prachi Gupta <prachig150904@gmail.com>
2025-03-07 13:55:51 +0530

    Uploaded login page with basic styling and layout

---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import {CssBaseline} from '@mui/material'//importing CssBaseline from material ui
+import {HelmetProvider} from 'react-helmet-async'//importing HelmetProvider from react-helmet-async
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <HelmetProvider>
+   <CssBaseline />
+   <App />
+   </HelmetProvider>
+  </StrictMode>,
+)
